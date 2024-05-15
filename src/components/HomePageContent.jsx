bcc819b63f071420d61b49178bff500e0eb592cf
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-05-14 18:25:06 -0600

    Se le agregan estilos a los post

--- src/components/HomePageContent.jsx
+++ src/components/HomePageContent.jsx
@@ -1,18 +1,19 @@
-
+// HomePageContent.jsx
 import React from 'react';
+
 
 function HomePageContent({ posts }) {
 
 
   return (
     <div>
-      <h2>Aquí se listan las publicaciones</h2>
+      <h2>Proyectos realizados</h2>
       <ul>
         {posts.map((post) => (
-          <li key={post._id} onClick={() => handlePostClick(post._id)}>
+          <li className="card" key={post._id}>
             <h3>{post.title}</h3>
             <p>{post.content}...</p>
-            <a href={post.url}>Ver más</a>
+            <a href={post.url}>Ver repositorio</a>
           </li>
         ))}
       </ul>
